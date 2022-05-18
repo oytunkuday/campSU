c2496b1dd735626938751400c72090f80ba7219a
borakdeniz <oytunkuday2001@hotmail.com>
2022-05-18 19:12:38 +0300

    Created  colors
    baslikta yazdigi gibi.

---
+++ lib/utils/colors.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Colors.lightBlue;
+  static const Color secondary = Color(0xFF80D8FF);
+
+  static const Color textColor = Color(0xFF424242);
+  static const Color headingColor = Color(0xFF0277BD);
+  static const Color captionColor = Colors.white30;
+  static const Color darkButtonTextColor = Colors.white;
+  static const Color appBarTitleTextColor = Colors.white;
+  static const Color textFieldFillColor = Color(0xFFD0D0D0);
+
+
+  static const Color backgroundColor = Color(0xFFFFF8E1);
+  static const Color headingColor = Color(0xFFFFA000);
+  static const Color postColor = Color(0xFFFFC107);
+  static const Color searchResultsColor = Color(0xFFFFECB3);
+  static const Color greyColor = Color(0xFFC0C0C1);
+}
