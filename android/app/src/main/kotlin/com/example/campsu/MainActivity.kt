8e187454edfeb28a072a6d5879ab169ae60c9160
borakdeniz <oytunkuday2001@hotmail.com>
2022-05-18 19:02:22 +0300

    gercek ilk commit
    bu sefer olur ins

---
+++ android/app/src/main/kotlin/com/example/campsu/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.campsu
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\Users\oytun\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\oytun\AndroidStudioProjects\campSU"
+export "FLUTTER_APPLICATION_PATH=C:\Users\oytun\AndroidStudioProjects\campsu"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
