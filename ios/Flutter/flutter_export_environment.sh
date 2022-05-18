e8b3b6dfadb161465ffd6ef089ea1ed1d40472f8
bengisuandic <bengisuandic@sabanciuniv.edu>
2022-05-18 19:32:22 +0300

    Dimension class added

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Users\oytun\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\oytun\AndroidStudioProjects\campsu"
+export "FLUTTER_ROOT=C:\Users\temp\Documents\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\temp\Desktop\CS310-Project\campSU"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
---
+++ lib/utils/dimensions.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Dimension {
+  static const double parentMargin = 20.0;
+  static const double regularMargin = 10.0;
+
+  static get parentPadding => const EdgeInsets.all(parentMargin);
+  static get regularPadding => const EdgeInsets.all(regularMargin);
+
+}
