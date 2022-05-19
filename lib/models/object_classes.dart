19ee740d30c1512768c4a830a13805694fcd0a75
oytunkuday <oytunkuday2001@hotmail.com>
2022-05-19 16:04:09 +0300

    bikac degisiklik
    bora her seyi utilse atma

--- lib/models/object_classes.dart
+++ lib/models/object_classes.dart
@@ -1,46 +1,37 @@
-import 'package:flutter/material.dart';
+//import 'package:flutter/material.dart';
 
-
-class userInfo{
+class UserInfo {
   var userName;
-  var password;
-  var email;
   var numFollowers;
   var followerArray = [];
   var numFollowed;
   var followedArray = [];
   var nmPosts;
   var profilePic;
-  var biography;
+  var biology;
 
-  userInfo({
-    required this.userName,
-    required this.email,
-    required this.password,
-  });
-
-  showUser(){
+  showUser() {
     print('User Name is: ${this.userName}');
     print('Number of followers is: ${this.numFollowers}');
     print('Number of followed people is: ${this.numFollowed}');
-    print('Number of posts is: ${this.nmPost}');
-    print('Users biography is ${this.biography}');
+    print('Number of posts is: ${this.nmPosts}');
+    print('Users biology is ${this.biology}');
     print('URL of the profile picture is: ${this.profilePic}');
   }
 
-  showFollowers(){
+  showFollowers() {
     print(followerArray);
   }
 
-  showFollowed(){
+  showFollowed() {
     print(followedArray);
   }
 }
 
-class postInfo{
+class postInfo {
   var nmLikes;
   var likesArray = [];
-  var nmcomments;
+  var nmComments;
   var commentArray = [];
   var pictureURL;
 
@@ -50,51 +41,54 @@ class postInfo{
     print('URL of the picture: ${this.pictureURL}');
   }
 
-  showCommenters(){
+  showCommenters() {
     print(this.commentArray);
   }
 
-  showLikers(){
+  showLikers() {
     print(this.likesArray);
   }
-
+  
 }
 
-class searchResult(){
-var searchedWords;
-var searchStorage = [];
+class searchResult {
+  var searchedWords;
+  var searchStorage = [];
 
-addtoStorage(var searched){
-this.searchStorage.add(searched);
+  addtoStorage(var searched) {
+    this.searchStorage.add(searched);
+  }
 }
 
-}
+class notification {
+  var username;
+  var event;
 
-class notification(){
-var username;
-var event;
+  notify() {
+    switch (this.event) {
+      case 'like':
+        {
+          print('${this.username} liked your post.');
+        }
+        break;
 
-notify(){
-switch(this.event){
-case 'like':{
-print('${this.username} liked your post.');
-}
-break;
+      case 'comment':
+        {
+          print('${this.username} has commented on your post.');
+        }
+        break;
 
-case 'comment':{
-print('${this.username} has commented on your post.');
-}
-break;
+      case 'follow':
+        {
+          print('${this.username} followed you.');
+        }
+        break;
 
-case 'follow':{
-print('${this.username} followed you.')
-}
-break;
-
-case 'mention':{
-print('${this.username} has mentioned you in a post.')
-}
-break;
-}
-}
+      case 'mention':
+        {
+          print('${this.username} has mentioned you in a post.');
+        }
+        break;
+    }
+  }
 }
